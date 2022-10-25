447e0cef13c036425cd6f04ae5674e1e7612fc8e
Yash Jain <yashjain31@gmail.com>
2022-10-25 23:11:02 +0100

    first commit for message project

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, NotFoundException } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+
+    constructor(public messagesService: MessagesService){}
+
+
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+
+    @Post()
+    createMessages(@Body() body: CreateMessageDto) {
+       return this.messagesService.create(body.content);
+    }
+
+    @Get('/:id')
+   async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
+
+        if(!message){
+            throw new NotFoundException('Message not found');
+        }
+
+        return message;
+    }
+
+
+}
